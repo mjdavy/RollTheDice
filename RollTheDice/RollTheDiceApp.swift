caab3fceffb99fe2c137c047d693f7a6cc4f66e3
Martin Davy <martin.davy@gmail.com>
2024-05-03 10:04:52 -0400

    initial commit

---
+++ RollTheDice/RollTheDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  RollTheDiceApp.swift
+//  RollTheDice
+//
+//  Created by Martin Davy on 5/3/24.
+//
+
+import SwiftUI
+
+@main
+struct RollTheDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
